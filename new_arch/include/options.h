c92abb50bcf21e6cb011959e0f37ee3736bcd556
Russell Dunk <russelldunk@gmail.com>
2018-03-21 09:25:59 -0400

    Starting to work on sizeing

---
+++ new_arch/include/options.h
@@ -0,0 +1,15 @@
+#ifndef LIBDRAW_OPTIONS_H
+#define LIBDRAW_OPTIONS_H
+
+#include <vector>
+#include <ios>
+
+class Options
+{
+public:
+    bool preserveAspectRatio = false;
+    int height = -1;
+    int width = -1;
+};
+
+#endif //LIBDRAW_OPTIONS_H
