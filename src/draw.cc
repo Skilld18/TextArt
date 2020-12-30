fdc70ddff2adb285b7d9fff874cc9dee7bf546e4
Russell Dunk <russell.dunk@gmail.com>
2020-12-30 09:32:24 -0500

    Add a basic black white draw call

--- src/draw.cc
+++ src/draw.cc
@@ -1,4 +1,8 @@
 #include <draw/draw.h>
+#include <iostream>
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/imgcodecs.hpp>
+#include <opencv4/opencv2/imgproc.hpp>
 
 int draw(Options options, std::string Filename)
 {
@@ -10,5 +14,26 @@ int draw(Options options, std::string Filename)
     {
         return 4;
     }
+    cv::Mat img;
+    img = cv::imread(Filename);
+    resize(img, img, cv::Size(static_cast<int>(options.get_width()), static_cast<int>(options.get_height())));
+
+    for (int i=0; i<img.rows; i++)
+    {
+        for(int j=0; j<img.cols; j++) 
+        {
+            const auto pixel = img.at<cv::Vec3b>(i,j);
+            if (pixel[0] + pixel[1] + pixel[2] < 430)
+            {
+               std::cout << 'c';
+            }
+            else
+            {
+                std::cout << ' ';
+            }
+        }
+        std::cout << "\n";
+    }
+
     return 0;
 }
