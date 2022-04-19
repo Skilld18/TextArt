a6f9602acc892f49585a2a7a3410fb7de625d580
Russell <russell.dunk@gmail.com>
2022-04-18 20:27:12 -0400

    Allowing read from stdin using tmp file

--- apps/main.cc
+++ apps/main.cc
@@ -1,12 +1,22 @@
 #include <draw/draw.h>
 #include <iostream>
+#include <fstream>
+
 
 int main(int argc, const char *argv[])
 {
-    if (argc < 2)
+    if ( argc > 1 )
     {
-        std::cerr << "Requires a file" << std::endl;
-        return 1;
-    }
-    return draw(argv[1]);
+      return draw(argv[1]);
+   }
+   else
+   {
+       std::ofstream myfile;
+       myfile.open ("/tmp/draw.txt");
+       for (std::string line; std::getline(std::cin, line);) {
+         myfile << line << "\n";
+       }
+       myfile.close();
+       return draw("/tmp/draw.txt");
+   }
 }
